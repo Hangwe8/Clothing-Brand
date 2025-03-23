3bd4ce40c0f77b3066c09db643f7f94097e5e4bb
Hangwe <hangwe00@gmail.com>
2025-03-23 22:26:16 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const menuItems = document.querySelectorAll(".menuItem");
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
         wrapper.style.transform = `translateX(${-100 * index}vw)`
-    })
-})
+    });
+});
